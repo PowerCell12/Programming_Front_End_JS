7094073438b55ce986c8de79d181fe63f7e38b1a
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-06-22 11:11:58 +0300

    Create 02_Print_every_N_th_Element_from_an_Array.js

---
+++ JS_Arrays_and_Strings/Exercise/02_Print_every_N_th_Element_from_an_Array.js
@@ -0,0 +1,18 @@
+function every_n_th(array, number){
+
+
+    let final = [];
+
+    for (let i = 0; i < array.length; i = i + number){
+
+        final.push(array[i])
+
+    }
+
+
+
+
+
+
+    return final;
+}
