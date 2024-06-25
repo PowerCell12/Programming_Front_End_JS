bfba4b17a64b2a77542f1325ed15875a2d6c916e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-06-25 17:32:47 +0300

    Create 01_Smallest_of_Three_Numbers.js

---
+++ Functions_and_Statements/Exercises/01_Smallest_of_Three_Numbers.js
@@ -0,0 +1,24 @@
+function smallest_three(num1, num2, num3){
+
+
+  const func = function(num1, num2, num3){
+
+    if (num1  < num2 && num1 < num3){
+      return num1
+    }
+    else if (num2 < num1 && num2 < num3){
+      return num2
+    }
+    else if (num3 < num1 && num3 < num2){
+      return num3
+    }
+    else{
+      return num1
+    }
+    
+  }
+
+  return func(num1, num2, num3)
+  
+  
+}
