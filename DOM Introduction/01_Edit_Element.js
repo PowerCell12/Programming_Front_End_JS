9d065ea45b4cf65ea4620cd13550b1a4a31b4293
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-07-05 14:48:15 +0300

    Delete DOM Introduction/01_Edit_Element.js

--- DOM Introduction/01_Edit_Element.js
+++
@@ -1,6 +0,0 @@
-function edit(first, second, third) {
-    while (first.textContent !== first.textContent.replace(second,third)){
-        first.textContent = first.textContent.replace(second, third);
-    }
-
-}
