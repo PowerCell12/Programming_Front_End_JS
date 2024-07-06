20df0eac9139e4ccb880ac755fa4cc8c6c76ef7c
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-07-06 11:42:58 +0300

    Create 04_Show_More.js

---
+++ DOM Introduction/Lab/04_Show_More.js
@@ -0,0 +1,9 @@
+function showText() {
+
+  let link = document.querySelector('#more');
+  link.style.display = 'none';
+
+  let text = document.querySelector("#text");
+  text.style.display = 'inline';
+  
+}
