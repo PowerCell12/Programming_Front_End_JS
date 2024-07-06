cac5f304c1ab309e4c50fe099d2c67054c0d7a08
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-07-06 11:42:42 +0300

    Create 03_Sum_Numbers.js

---
+++ DOM Introduction/Lab/03_Sum_Numbers.js
@@ -0,0 +1,10 @@
+function calc() {
+
+  let first  = document.getElementById('num1');
+  let second = document.getElementById('num2')
+
+  let third = document.getElementById('sum');
+
+  third.value = Number(first.value) + Number(second.value);
+  
+}
